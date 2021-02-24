e3931cc74b5f392e151d254984692f499ea55f2d
Timothy S. Jones <tim@empty.sh>
2021-02-23 23:26:24 -0500

    Set font.

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]        = "monospace:size=9";
+static const char font[]        = "Source Code Pro:pixelsize=14:antialias=true:autohint=true";
 static const char* normbgcolor  = "#222222";
 static const char* normfgcolor  = "#cccccc";
 static const char* selbgcolor   = "#555555";
